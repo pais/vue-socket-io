86b385243dafd7f8fcea22e48696ac375ad3501d
Ahmet Bekman <ahmet.bekman@inventiv.com.tr>
2023-01-07 01:28:56 +0300

    node server rabbitmq consumer implementation

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,51 @@
 <template>
-    <Pie :data="data" :options="options" />
-  </template>
-  
-  <script lang="ts">
-  import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
-  import { Pie } from 'vue-chartjs'
-  import { io } from "socket.io-client";
-  import * as chartConfig from './chartConfig.js'
-  
-  ChartJS.register(ArcElement, Tooltip, Legend)
+  <Pie :data="data" :options="options" />
+</template>
 
-  const socket = io("https://localhost:3000", {
-    path: "/vote/"
-  });
-  
-  export default {
-    name: 'App',
-    components: {
-      Pie,
-      io
-    },
-    data() {
-      return chartConfig
-    }
+<script lang="ts">
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
+import { Pie } from 'vue-chartjs'
+import { io } from "socket.io-client";
+import * as chartConfig from './chartConfig.js'
+
+ChartJS.register(ArcElement, Tooltip, Legend)
+
+const socket = io("localhost:3000", {
+  path: "/vote/",
+  transports: ['websocket', 'polling'],
+  withCredentials: true
+});
+
+socket.on("connect_error", () => {
+  // revert to classic upgrade
+  debugger;
+  socket.io.opts.transports = ["polling", "websocket"];
+});
+
+socket.on("connect", () => {
+  console.log(socket.id); // x8WIv7-mJelg7on_ALbx
+});
+
+socket.on("started", (message) => {
+  console.log(message);
+});
+
+socket.on("polling", (message) => {
+  console.log(message);
+});
+
+export default {
+  name: 'App',
+  components: {
+    Pie,
+    io
+  },
+  data() {
+    return chartConfig
+  },
+  created() {
+    socket.connect();
   }
-  </script>
-  
+
+}
+</script>
--- server/server.js
+++ server/server.js
@@ -1,13 +1,34 @@
 import { createServer } from "http";
 import { Server } from "socket.io";
+import * as amqp from "amqplib";
 
 const httpServer = createServer();
 const io = new Server(httpServer, {
-  // ...
+  path: "/vote/",
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"]
+  },
+  transports: ['websocket', 'polling'],
+  credentials: true
 });
 
 io.on("connection", (socket) => {
-  debugger;
+  socket.broadcast.emit("started", "lets go");
+  console.log(socket.handshake.query);
 });
+
+connect_rabbitmq()
+
+async function connect_rabbitmq() {
+  const connection = await amqp.connect('amqp://localhost')
+  const channel = await connection.createChannel()
+
+  channel.consume("PollingQueue", data => {
+    console.log("gelen oy: ", JSON.parse(data.content.toString()));
+    io.emit("polling", JSON.parse(data.content.toString()));
+    channel.ack(data);
+  })
+}
 
 httpServer.listen(3000);
