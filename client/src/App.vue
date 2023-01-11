b7cfea9ee703b1648934f29e3a6e355d7cf2236b
Ahmet Bekman <ahmet.bekman@inventiv.com.tr>
2023-01-12 00:01:51 +0300

    realtime update chart

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,38 +1,14 @@
 <template>
-  <Pie :data="data" :options="options" />
+  <Pie id="pie-chart" :data="chartData" :options="options" />
 </template>
 
 <script lang="ts">
+
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
 import { Pie } from 'vue-chartjs'
 import { io } from "socket.io-client";
-import * as chartConfig from './chartConfig.js'
 
 ChartJS.register(ArcElement, Tooltip, Legend)
-
-const socket = io("localhost:3000", {
-  path: "/vote/",
-  transports: ['websocket', 'polling'],
-  withCredentials: true
-});
-
-socket.on("connect_error", () => {
-  // revert to classic upgrade
-  debugger;
-  socket.io.opts.transports = ["polling", "websocket"];
-});
-
-socket.on("connect", () => {
-  console.log(socket.id); // x8WIv7-mJelg7on_ALbx
-});
-
-socket.on("started", (message) => {
-  console.log(message);
-});
-
-socket.on("polling", (message) => {
-  console.log(message);
-});
 
 export default {
   name: 'App',
@@ -41,10 +17,80 @@ export default {
     io
   },
   data() {
-    return chartConfig
+    return {
+      socket: {},
+      chartData: {
+        labels: [],
+        datasets: [
+          {
+            backgroundColor: ["#41B883", "#E46651", "#00D8FF", '#FFB833'],
+            data: []
+          }]
+      },
+      options: {
+        legend: {
+          display: true
+        },
+        responsive: true,
+        maintainAspectRatio: false,
+        title: {
+          display: true,
+          text: 'test'
+        }
+      }
+    }
   },
   created() {
-    socket.connect();
+    this.socket = io("localhost:3000", {
+      path: "/vote/",
+      transports: ['websocket', 'polling'],
+      withCredentials: true
+    });
+
+    this.socket.on("connect_error", () => {
+      this.socket.io.opts.transports = ["polling", "websocket"];
+    });
+
+    this.socket.on("connect", () => {
+      console.log(this.socket.id); // x8WIv7-mJelg7on_ALbx
+    });
+
+    this.socket.on("started", (message) => {
+      console.log(message);
+    });
+
+    var self = this;
+
+    this.socket.on("polling", (message) => {
+      self.updateChart(message);
+      console.log(message);
+    });
+
+    this.socket.connect();
+  },
+  methods: {
+    updateChart(message) {
+      var self = this;
+
+      const labels: string[] = [];
+      const datas: Int32Array[] = [];
+
+      for (var i = 0; i < message.Details.length; i++) {
+        datas.push(message.Details[i].VoteCount);
+        labels.push(message.Details[i].CandidateName);
+        console.log(message.Details[i]);
+      }
+
+      self.chartData.datasets[0].data = datas;
+      self.chartData.labels = labels;
+      self.options.title.text = message.DateTime;
+
+      var pieChart = ChartJS.getChart("pie-chart");
+
+      pieChart.data = self.chartData;
+      pieChart.update()
+      pieChart.render();
+    }
   }
 
 }
